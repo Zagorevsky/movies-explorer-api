bad8d46c0344dc8152061c31f7361037c10e0bf3
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-03-09 12:05:21 +0300

    fix: userSchema

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 30,
     default: 'Александр',
-    unique: true,
+    required: true,
   },
   email: {
     type: String,
